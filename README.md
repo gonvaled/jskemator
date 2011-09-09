e6e0573f3ddc7352ce60180939b41f3e31ddbfe0
Daniel Gonzalez <gonvaled@gonvaled.com>
2011-09-09 15:23:54 +0200

    Already possible to create basic JSON schemas. Not possible to reuse schema information yet. Lots of functionality not implemented yet.

---
+++ README.md
@@ -0,0 +1,6 @@
+This tool takes as input json data and produces an skeleton of a corresponding json schema.
+Most parameters are necessarily arbitrary, since they can not be inferred from the json data.
+
+## Use
+
+jskemator.py -f examples/example.json
---
+++ jskemator.py
@@ -0,0 +1,126 @@
+"""
+Take JSON data and generate a skeleton for the JSON schema which represents it
+
+Usage:
+  jskemator.py [-f|--filename <filename>] [-h|--help]
+
+"""
+import simplejson as json
+import pprint
+import sys
+import getopt
+
+pp = pprint.PrettyPrinter(indent=4)
+
+class Jskemator:
+    def __ini__(self):
+        pass
+    def load(self, json_str):
+        self.obj = json.loads(json_str)
+    def _skemateDict(self, d):
+        skema = { }
+        skema['description'] = 'Dummy description'
+        skema['type'] = 'object'
+        skema['properties'] = { }
+        for key, value in d.items ():
+            skema['properties'][key] = self._skemate(value)
+        skema['additionalProperties'] = False
+        return skema
+    def _skemateList(self, l):
+        skema = { } 
+        skema['description'] = 'Dummy list description'
+        skema['type'] = 'array'
+        skema['properties'] = [ ]
+        for value in l:
+            skema['properties'].append(self._skemate(value)) 
+        skema['additionalProperties'] = False
+        return skema
+    def _skemateStr(self, str):
+        res = {}
+        res['description'] = 'Dummy string description'
+        res['type'] = 'string'
+        res['required'] = True
+        res['pattern'] = ''
+        res['value'] = str
+        return res
+    def _skemateInt(self, i):
+        res = {}
+        res['description'] = 'Dummy int description'
+        res['type'] = 'integer'
+        res['required'] = True
+        res['pattern'] = ''
+        res['value'] = i
+        return res
+    def _skemateFloat(self, f):
+        res = {}
+        res['description'] = 'Dummy float description'
+        res['type'] = 'float'
+        res['required'] = True
+        res['pattern'] = ''
+        res['value'] = f
+        return res
+    def _skemate(self, o):
+        if isinstance(o, (list, tuple)):
+            return self._skemateList(o)
+        elif isinstance(o, dict):
+            return self._skemateDict(o)
+        elif isinstance(o, str):
+            return self._skemateStr(o)
+        elif isinstance(o, str):
+            return self._skemateStr(o)
+        elif isinstance(o, int):
+            return self._skemateInt(o)
+        elif isinstance(o, long):
+            return self._skemateLong(o)
+        elif isinstance(o, float):
+            return self._skemateFloat(o)
+        elif o == None:
+            return self._skemateNone(o)
+        elif o == False:
+            return self._skemateFalse(o)
+        elif o == True:
+            return self._skemateTrue(o)
+    def skemate(self):
+        return self._skemate(self.obj)
+        
+def usage():
+    print __doc__
+    
+def process_options(argv):
+  filename = None
+  h = False
+  try:
+    opts, args = getopt.getopt(argv[1:], "f:h", ['filename=', 'help'])
+  except getopt.GetoptError, err:
+    # print help information and exit:
+    print str(err)
+    usage()
+    sys.exit(2)
+  for o, a in opts:
+    if o in ("-f", "--filename"):
+      filename = a
+    elif o in ("-h", "--help"):
+      h = True
+  return filename, h
+
+def main():
+    filename, h = process_options(sys.argv)
+    if h or filename == None:
+        usage ()
+        sys.exit(0)
+    jskemator = Jskemator()
+    try:
+        h = open(filename)
+    except:
+        print "File %s can not be opened for reading" % (filename)
+        sys.exit(0)
+    json_str = h.read()
+    h.close()
+    jskemator.load(json_str)
+    #pp.pprint(jskemator.obj)
+    skema = jskemator.skemate()
+    #pp.pprint(skema)
+    print json.dumps(skema, indent=4)
+    
+if __name__ == '__main__':
+  main()
